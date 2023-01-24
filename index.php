b72c3d5c54265ab6c444c6251458e95025a1d5bb
ibrahim madi <ibrahim75madi@gmail.com>
2023-01-24 22:24:00 +0100

    Better Route

--- index.php
+++ index.php
@@ -8,19 +8,19 @@
 */
 
 include "controleurs/class.controleurUtilisateur.php";
+require_once "attributs/class.Route.php";
 
 // Crée une instance du contrôleur
 $controleur = new controleurUtilisateur();
 
-$action = isset($_GET['action']) ? $_GET['action'] : 'accueil';
+// $action = isset($_GET['action']) ? $_GET['action'] : 'accueil';
 
-// Appelle la méthode correspondante du contrôleur
-match ($action) {
-  'accueil' => $controleur->getPageAccueil(),
-  'profil' => $controleur->getPageProfil($_GET['id']),
-  'connexion' => $controleur->getPageConnexion(),
-  'inscription' => $controleur->getPageInscription(),
-  default => $controleur->getPage404(), // Affiche une erreur si l'action est inconnue
-};
-
+// // Appelle la méthode correspondante du contrôleur
+// match ($action) {
+//   'accueil' => $controleur->getPageAccueil(),
+//   'profil' => $controleur->getPageProfil($_GET['id']),
+//   'connexion' => $controleur->getPageConnexion(),
+//   'inscription' => $controleur->getPageInscription(),
+//   default => $controleur->getPage404(), // Affiche une erreur si l'action est inconnue
+// };
 
