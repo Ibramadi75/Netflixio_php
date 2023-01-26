7751b50edce6cf7fe4bdde9363fba09f83cdf9c2
ibrahim madi <ibrahim75madi@gmail.com>
2023-01-26 20:41:35 +0100

    correction bug bdd

--- public/index.php
+++ public/index.php
@@ -42,3 +42,12 @@ $bdd = new PdoApp();
     ?>
 </body>
 </html>
+
+<script>
+    function focused(el) {
+        // On récupère le label associé
+        el_label = document.getElementById(el + "_label");
+        el_label.style.fontSize = "12px";
+        el_label.style.top = "2px";
+    }
+</script>
